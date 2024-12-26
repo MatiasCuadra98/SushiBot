6630d535d15bd9f53e995ed2c49cf564ecbf3f37
Matías Cuadra <MatiasCuadra98>
2024-12-26 12:28:01 -0300

    Inicializamos servidor

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const routes = require('./src/routes');
+require('dotenv').config();
+
+
+
+const app = express();
+const PORT = process.env.PORT;
+
+// Middleware
+app.use(bodyParser.json());
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// Rutas
+app.use('/', routes);
+
+// Ruta principal
+app.get('/', (req, res) => {
+    res.send('¡Bienvenido al chatbot de sushi!');
+});
+
+// Iniciar servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
